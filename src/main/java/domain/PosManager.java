e4f56b2be6c939b34aa50d4c50e6c03e81fbc75c
lns13301 <rkdrbgks22@ks.ac.kr>
2020-12-15 16:29:00 +0900

    feat: 프로그램의 상태를 추가, 프로그램 진행 추가

--- src/main/java/domain/PosManager.java
+++ src/main/java/domain/PosManager.java
@@ -4,28 +4,68 @@ import view.InputView;
 import view.OutputView;
 
 public class PosManager {
-    public void showMainScene() {
+    public void onPos() {
+        State state = State.MAIN_SCENE;
+
+        do {
+            state = showMainScene(state);
+            state = showTables(state);
+            state = showPaymentScene(state);
+        } while (!state.equals(State.QUIT));
+    }
+
+    public State showMainScene(State state) {
         OutputView.printMainScene();
-        InputView.inputChoiceFunction();
+        return getMainSceneState(state, InputView.inputChoiceFunction());
     }
 
-    public void showTables() {
-        OutputView.printTables(TableRepository.tables());
-        int tableNumber = InputView.inputTableNumber();
-        int menuNumber = InputView.inputRegisterMenu();
-        int count = InputView.inputCount();
+    private State getMainSceneState(State state, int input) {
+        if (input == 1) {
+            return State.ORDER_SCENE;
+        }
 
-        TableRepository.addOrder(tableNumber, menuNumber, count);
+        if (input == 2) {
+            return State.PAYMENT_SCENE;
+        }
+
+        if (input == 3) {
+            return State.QUIT;
+        }
+
+        return state;
     }
 
-    public void showPaymentScene() {
-        OutputView.printTables(TableRepository.tables());
+    public State showTables(State state) {
+        if (state.equals(State.ORDER_SCENE)) {
+            OutputView.printTables(TableRepository.tables());
 
-        int tableNumber = InputView.inputTable();
+            int tableNumber = InputView.inputTableNumber();
+            int menuNumber = InputView.inputRegisterMenu();
+            int count = InputView.inputCount();
 
-        OutputView.printReceipt(TableRepository.orders(tableNumber));
-        boolean isCash = isCash(InputView.inputPayment(tableNumber));
-        OutputView.printTotalPayment(TableRepository.totalPaymentPrice(tableNumber, isCash));
+            TableRepository.addOrder(tableNumber, menuNumber, count);
+
+            return state;
+        }
+
+        return state;
+    }
+
+    public State showPaymentScene(State state) {
+        if (state.equals(State.PAYMENT_SCENE)) {
+
+            OutputView.printTables(TableRepository.tables());
+
+            int tableNumber = InputView.inputTable();
+
+            OutputView.printReceipt(TableRepository.orders(tableNumber));
+            boolean isCash = isCash(InputView.inputPayment(tableNumber));
+            OutputView.printTotalPayment(TableRepository.totalPaymentPrice(tableNumber, isCash));
+
+            return state;
+        }
+
+        return state;
     }
 
     private boolean isCash(int choice) {
---
+++ src/main/java/domain/State.java
@@ -0,0 +1,8 @@
+package domain;
+
+public enum State {
+    MAIN_SCENE,
+    ORDER_SCENE,
+    PAYMENT_SCENE,
+    QUIT
+}
