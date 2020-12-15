c3fac265fd7f4f024791085430d84a36310bfb70
lns13301 <rkdrbgks22@ks.ac.kr>
2020-12-15 15:30:19 +0900

    feat: 메인 화면 기능 추가

---
+++ src/main/java/domain/PosManager.java
@@ -0,0 +1,11 @@
+package domain;
+
+import view.InputView;
+import view.OutputView;
+
+public class PosManager {
+    public void showMainScene() {
+        OutputView.printMainScene();
+        InputView.inputChoiceFunction();
+    }
+}
